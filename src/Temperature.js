7ccb9b98351bd67654bab647c93e7b6c70c16127
Ha Thu <tranhathu6989@gmail.com>
2022-09-24 14:16:05 +0200

    Set Temperature

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
 export default function Temperature() {
-  return <div>Temperature</div>;
+  const [temperature, setTemp] = useState(10);
+
+  return (
+    <div>
+      <h1>Temperature</h1>
+      <input type="number" onChange={(e) => setTemp(e.target.value)} />
+      {temperature < 10 && <p style={{ color: "blue" }}>It's cold ❄️</p>}
+      {temperature >= 10 && temperature <= 30 && (
+        <p style={{ color: "black" }}>It's nice 🌼</p>
+      )}
+      {temperature > 30 && <p style={{ color: "red" }}>It's warm ☀️</p>}
+    </div>
+  );
 }
